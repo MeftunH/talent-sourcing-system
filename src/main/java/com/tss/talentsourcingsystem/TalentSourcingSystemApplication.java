5b52a0ff77af4c3a539c6ee25198f75d6fb91000
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-24 23:36:08 +0300

    Init Project

---
+++ src/main/java/com/tss/talentsourcingsystem/TalentSourcingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tss.talentsourcingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentSourcingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentSourcingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tss/talentsourcingsystem/TalentSourcingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tss.talentsourcingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentSourcingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
