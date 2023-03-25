741d15a3f296a1343a1a6f665c4434bf540d6e20
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-26 01:31:19 +0300

    add candidate status

--- src/main/java/com/tss/talentsourcingsystem/application/candidate/entity/Candidate.java
+++ src/main/java/com/tss/talentsourcingsystem/application/candidate/entity/Candidate.java
@@ -1,6 +1,7 @@
 package com.tss.talentsourcingsystem.application.candidate.entity;
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 
+import com.tss.talentsourcingsystem.application.candidate.enums.CandidateStatus;
 import com.tss.talentsourcingsystem.application.contactInformation.entity.ContactInformation;
 import com.tss.talentsourcingsystem.application.person.entity.Person;
 import jakarta.persistence.*;
@@ -20,4 +21,6 @@ public class Candidate extends Person {
 
     @OneToMany(mappedBy = "candidate", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<ContactInformation> contactInfo=new HashSet<>();
+
+    private CandidateStatus candidateStatus;
 }
