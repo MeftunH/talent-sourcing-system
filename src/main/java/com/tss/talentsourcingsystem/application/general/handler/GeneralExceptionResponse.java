256e5bc28cda0e2a769aa65c3f150dea48b142b8
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-25 03:33:12 +0300

    add general exception response

---
+++ src/main/java/com/tss/talentsourcingsystem/application/general/handler/GeneralExceptionResponse.java
@@ -0,0 +1,17 @@
+package com.tss.talentsourcingsystem.application.general.handler;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class GeneralExceptionResponse {
+
+    private Date errorDate;
+    private String message;
+    private String detail;
+}
+
