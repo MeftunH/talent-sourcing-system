3602284b672dab30556c186cd5cebd76d2972046
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-27 17:42:32 +0300

    set column annotations

--- src/main/java/com/tss/talentsourcingsystem/application/contactInformation/entity/ContactInformation.java
+++ src/main/java/com/tss/talentsourcingsystem/application/contactInformation/entity/ContactInformation.java
@@ -3,19 +3,23 @@ package com.tss.talentsourcingsystem.application.contactInformation.entity;
 
 import com.tss.talentsourcingsystem.application.candidate.entity.Candidate;
 import com.tss.talentsourcingsystem.application.contactInformation.enums.ContactInformationType;
+import com.tss.talentsourcingsystem.application.generic.entity.BaseEntity;
 import jakarta.persistence.*;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "contact_type")
-public abstract class ContactInformation {
+public abstract class ContactInformation extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name = "contactInfoSeq", sequenceName = "contact_info_id_sec")
+    @GeneratedValue(generator="contactInfoSeq")
     private Long id;
     @Enumerated(EnumType.STRING)
+    @Column(name = "contact_information_type", nullable = false, length = 30)
     private ContactInformationType contactInformationType;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "candidate_id", nullable = false)
     private Candidate candidate;
 
 }
