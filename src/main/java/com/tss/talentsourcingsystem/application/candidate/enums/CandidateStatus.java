2d6a8eb79a8999819a9d17d5110a131322d99710
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-27 17:42:43 +0300

    set status types

--- src/main/java/com/tss/talentsourcingsystem/application/candidate/enums/CandidateStatus.java
+++ src/main/java/com/tss/talentsourcingsystem/application/candidate/enums/CandidateStatus.java
@@ -2,4 +2,8 @@ package com.tss.talentsourcingsystem.application.candidate.enums;
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 
 public enum CandidateStatus {
+    SOURCED,
+    INTERVIEWING,
+    OFFER_SEND,
+    HIRED
 }
