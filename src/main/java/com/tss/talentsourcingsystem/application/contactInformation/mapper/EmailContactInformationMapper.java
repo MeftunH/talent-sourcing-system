e408c86f91a48f1efa1ca2cbfe9b4ef850f9c08a
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-28 22:26:53 +0300

    add EmailContactInformation

---
+++ src/main/java/com/tss/talentsourcingsystem/application/contactInformation/mapper/EmailContactInformationMapper.java
@@ -0,0 +1,10 @@
+package com.tss.talentsourcingsystem.application.contactInformation.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
+
+public class EmailContactInformationMapper {
+}
