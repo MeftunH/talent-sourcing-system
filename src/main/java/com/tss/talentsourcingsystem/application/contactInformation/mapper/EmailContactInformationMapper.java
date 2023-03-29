fa7895ee93f33e9e72e5a276395930e916982d32
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-29 23:24:29 +0300

    set singleton pattern

--- src/main/java/com/tss/talentsourcingsystem/application/contactInformation/mapper/EmailContactInformationMapper.java
+++ src/main/java/com/tss/talentsourcingsystem/application/contactInformation/mapper/EmailContactInformationMapper.java
@@ -1,10 +1,18 @@
 package com.tss.talentsourcingsystem.application.contactInformation.mapper;
 
+import com.tss.talentsourcingsystem.application.contactInformation.dto.ContactInformationDto;
+import com.tss.talentsourcingsystem.application.contactInformation.dto.ContactInformationSaveRequestDto;
+import com.tss.talentsourcingsystem.application.contactInformation.entity.ContactInformation;
+import com.tss.talentsourcingsystem.application.contactInformation.entity.EmailContactInformation;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
 
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
+public interface EmailContactInformationMapper {
+    EmailContactInformationMapper INSTANCE = Mappers.getMapper(EmailContactInformationMapper.class);
+    EmailContactInformation contactInformationSaveRequestDtoToContactInformation(ContactInformationSaveRequestDto contactInformationSaveRequestDto);
 
-public class EmailContactInformationMapper {
+    ContactInformationDto contactInformationToContactInformationDto(ContactInformation savedContactInformation);
 }
