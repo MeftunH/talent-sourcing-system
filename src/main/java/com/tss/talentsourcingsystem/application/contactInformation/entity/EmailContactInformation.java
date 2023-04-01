0d66e9e2cba5cce94122beee9b2ac80bd8c02f63
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-01 12:37:09 +0300

    remove unnecessary imports

--- src/main/java/com/tss/talentsourcingsystem/application/contactInformation/entity/EmailContactInformation.java
+++ src/main/java/com/tss/talentsourcingsystem/application/contactInformation/entity/EmailContactInformation.java
@@ -1,14 +1,11 @@
 package com.tss.talentsourcingsystem.application.contactInformation.entity;
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 
-import com.tss.talentsourcingsystem.application.candidate.entity.Candidate;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 
 @Entity
 @DiscriminatorValue(value="EMAIL")
