cdd8bda33b935e17c24cd51338d6c313d136d7d1
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-02 20:09:29 +0300

    add validateCandidateStatusIsExists

--- src/main/java/com/tss/talentsourcingsystem/application/candidate/validation/impl/CandidateValidationServiceImpl.java
+++ src/main/java/com/tss/talentsourcingsystem/application/candidate/validation/impl/CandidateValidationServiceImpl.java
@@ -3,16 +3,18 @@ package com.tss.talentsourcingsystem.application.candidate.validation.impl;
 
 import com.tss.talentsourcingsystem.application.candidate.entity.Candidate;
 import com.tss.talentsourcingsystem.application.candidate.enums.CandidateErrorMessage;
+import com.tss.talentsourcingsystem.application.candidate.enums.CandidateStatus;
 import com.tss.talentsourcingsystem.application.candidate.repository.CandidateRepository;
 import com.tss.talentsourcingsystem.application.candidate.validation.CandidateValidationService;
-import com.tss.talentsourcingsystem.application.general.exception.GeneralBusinessException;
 import com.tss.talentsourcingsystem.application.general.exception.IllegalFieldException;
+import com.tss.talentsourcingsystem.application.general.exception.ItemNotFoundException;
 import com.tss.talentsourcingsystem.application.person.entity.Person;
 import com.tss.talentsourcingsystem.application.person.enums.PersonErrorMessage;
 import com.tss.talentsourcingsystem.application.person.enums.PersonType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
+import java.util.EnumSet;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -28,13 +30,13 @@ public class CandidateValidationServiceImpl implements CandidateValidationServic
     @Override
     public void checkCandidateExists(Candidate candidate) {
         if (candidate==null) {
-            throw new GeneralBusinessException(CandidateErrorMessage.CANDIDATE_NOT_FOUND);
+            throw new ItemNotFoundException(CandidateErrorMessage.CANDIDATE_NOT_FOUND);
         }
     }
     @Override
     public void checkOptionalCandidateExists(Optional<Person> candidateOptional) {
         if (candidateOptional.isEmpty()) {
-            throw new GeneralBusinessException(CandidateErrorMessage.CANDIDATE_NOT_FOUND);
+            throw new ItemNotFoundException(CandidateErrorMessage.CANDIDATE_NOT_FOUND);
         }
     }
 
@@ -44,6 +46,15 @@ public class CandidateValidationServiceImpl implements CandidateValidationServic
         validateIsPersonTypeCandidate(candidate);
 
     }
+
+    @Override
+    public void validateCandidateStatusIsExists(CandidateStatus candidateStatus) {
+       if(!EnumSet.allOf(CandidateStatus.class).contains(candidateStatus))
+       {
+              throw new IllegalFieldException(CandidateErrorMessage.CANDIDATE_STATUS_NOT_FOUND);
+       }
+    }
+
     private void validateAreFieldsNonNull(Candidate candidate) {
         if (Objects.isNull(candidate.getName())||Objects.isNull(candidate.getSurname())||Objects.isNull(candidate.getCandidateStatus())||Objects.isNull(candidate.getPersonType())) {
             throw new IllegalFieldException(PersonErrorMessage.FIELD_CANNOT_BE_NULL);
