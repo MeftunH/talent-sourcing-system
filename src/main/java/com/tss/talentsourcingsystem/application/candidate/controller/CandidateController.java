1e9c833caa5a016985c65f2c107d4dafbe2b0ecb
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-02 20:07:20 +0300

    add updateCandidateStatus and deleteCandidate functionalities

--- src/main/java/com/tss/talentsourcingsystem/application/candidate/controller/CandidateController.java
+++ src/main/java/com/tss/talentsourcingsystem/application/candidate/controller/CandidateController.java
@@ -4,6 +4,7 @@ package com.tss.talentsourcingsystem.application.candidate.controller;
 import com.tss.talentsourcingsystem.application.candidate.dto.CandidateDto;
 import com.tss.talentsourcingsystem.application.candidate.dto.CandidateSaveRequestDto;
 import com.tss.talentsourcingsystem.application.candidate.dto.CandidateUpdateRequestDto;
+import com.tss.talentsourcingsystem.application.candidate.dto.CandidateUpdateStatusRequestDto;
 import com.tss.talentsourcingsystem.application.candidate.service.CandidateService;
 import com.tss.talentsourcingsystem.application.generic.dto.RestResponse;
 import org.springframework.http.ResponseEntity;
@@ -20,18 +21,31 @@ public class CandidateController {
 
     @PostMapping
     public ResponseEntity<RestResponse<CandidateDto>> saveCandidate(@RequestBody CandidateSaveRequestDto candidateSaveRequestDto) {
-        CandidateDto candidateDto = candidateService.saveCandidate(candidateSaveRequestDto);
+        CandidateDto candidateDto=candidateService.saveCandidate(candidateSaveRequestDto);
         return ResponseEntity.ok(RestResponse.of(candidateDto));
     }
+
     @GetMapping("/{candidateId}")
     public ResponseEntity<RestResponse<CandidateDto>> getCandidateById(@PathVariable Long candidateId) {
-        CandidateDto candidateDto = candidateService.getCandidateDtoById(candidateId);
+        CandidateDto candidateDto=candidateService.getCandidateDtoById(candidateId);
         return ResponseEntity.ok(RestResponse.of(candidateDto));
     }
 
     @PutMapping("/{candidateId}")
     public ResponseEntity<RestResponse<CandidateDto>> updateCandidate(@PathVariable Long candidateId, @RequestBody CandidateUpdateRequestDto candidateUpdateRequestDto) {
-        CandidateDto candidateDto = candidateService.updateCandidate(candidateId, candidateUpdateRequestDto);
+        CandidateDto candidateDto=candidateService.updateCandidate(candidateId, candidateUpdateRequestDto);
+        return ResponseEntity.ok(RestResponse.of(candidateDto));
+    }
+
+    @DeleteMapping("/{candidateId}")
+    public ResponseEntity<RestResponse<CandidateDto>> deleteCandidate(@PathVariable Long candidateId) {
+        candidateService.deleteCandidate(candidateId);
+        return ResponseEntity.ok(RestResponse.empty());
+    }
+
+    @PatchMapping("/{candidateId}/status")
+    public ResponseEntity<RestResponse<CandidateDto>> updateCandidateStatus(@PathVariable Long candidateId, @RequestBody CandidateUpdateStatusRequestDto candidateUpdateStatusRequestDto) {
+        CandidateDto candidateDto=candidateService.updateCandidateStatus(candidateId, candidateUpdateStatusRequestDto);
         return ResponseEntity.ok(RestResponse.of(candidateDto));
     }
 }
