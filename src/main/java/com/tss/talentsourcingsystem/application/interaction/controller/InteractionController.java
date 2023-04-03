b1009d259964e0deba90c7ff8615260c5f7bd436
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-03 23:26:11 +0300

    fix RequestMapping

--- src/main/java/com/tss/talentsourcingsystem/application/interaction/controller/InteractionController.java
+++ src/main/java/com/tss/talentsourcingsystem/application/interaction/controller/InteractionController.java
@@ -13,29 +13,39 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/interactions")
+@RequestMapping("/api/v1/interactions")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class InteractionController {
     private final InteractionService interactionService;
+
     @PostMapping
     public ResponseEntity<RestResponse<InteractionDto>> saveInteraction(@RequestBody InteractionSaveRequestDto interactionSaveRequest) {
-        InteractionDto interactionDto = interactionService.saveInteraction(interactionSaveRequest);
+        InteractionDto interactionDto=interactionService.saveInteraction(interactionSaveRequest);
         return ResponseEntity.ok(RestResponse.of(interactionDto));
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<RestResponse<InteractionDto>> getInteractionById(@PathVariable Long id) {
-        InteractionDto interactionDto = interactionService.getInteractionById(id);
+        InteractionDto interactionDto=interactionService.getInteractionById(id);
         return ResponseEntity.ok(RestResponse.of(interactionDto));
     }
+
     @GetMapping("/candidate/{id}")
     public ResponseEntity<RestResponse<List<InteractionDto>>> getInteractionsByCandidateId(@PathVariable Long id) {
-        List<InteractionDto> interactionDtoList = interactionService.getInteractionsByCandidateId(id);
+        List<InteractionDto> interactionDtoList=interactionService.getInteractionsByCandidateId(id);
         return ResponseEntity.ok(RestResponse.of(interactionDtoList));
     }
+
     @PutMapping("/{id}")
     public ResponseEntity<RestResponse<InteractionDto>> updateInteraction(@PathVariable Long id, @RequestBody InteractionUpdateRequestDto interactionUpdateRequestDto) {
-        InteractionDto interactionDto = interactionService.updateInteraction(id, interactionUpdateRequestDto);
+        InteractionDto interactionDto=interactionService.updateInteraction(id, interactionUpdateRequestDto);
         return ResponseEntity.ok(RestResponse.of(interactionDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<RestResponse<Void>> deleteInteraction(@PathVariable Long id) {
+        interactionService.deleteInteraction(id);
+        return ResponseEntity.ok(RestResponse.empty());
     }
 }
