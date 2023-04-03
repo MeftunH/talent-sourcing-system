c6e7d678e1ab07fbab322bcbaabef70c922e4c68
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-03 23:17:29 +0300

    set phoneNumberContactInformationToContactInformationDto mappings

--- src/main/java/com/tss/talentsourcingsystem/application/contactInformation/mapper/PhoneNumberContactInformationMapper.java
+++ src/main/java/com/tss/talentsourcingsystem/application/contactInformation/mapper/PhoneNumberContactInformationMapper.java
@@ -2,14 +2,11 @@ package com.tss.talentsourcingsystem.application.contactInformation.mapper;
 
 import com.tss.talentsourcingsystem.application.contactInformation.dto.ContactInformationDto;
 import com.tss.talentsourcingsystem.application.contactInformation.dto.ContactInformationSaveRequestDto;
-import com.tss.talentsourcingsystem.application.contactInformation.entity.ContactInformation;
 import com.tss.talentsourcingsystem.application.contactInformation.entity.PhoneNumberContactInformation;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
-
-import java.util.List;
 
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 @Mapper(uses = ContactInformationMapper.class, unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
@@ -24,10 +21,9 @@ public interface PhoneNumberContactInformationMapper {
     @Mapping(source = "baseAdditionalFields.updatedDate", target = "baseAdditionalFieldsUpdatedDate")
     ContactInformationDto contactInformationToContactInformationDto(PhoneNumberContactInformation savedContactInformation);
 
-    List<ContactInformation> phoneNumberContactInformationListToContactInformationList(List<PhoneNumberContactInformation> phoneNumberContactInformationList);
-
-
-    ContactInformationDto phoneNumberContactInformationToContactInformationDtoList(PhoneNumberContactInformation phoneNumberContactInformation);
-
-    ContactInformationDto contactInformationToContactInformationDto(ContactInformationSaveRequestDto contactInformationSaveRequestDto);
+    @Mapping(source = "candidate.id", target = "candidateId")
+    @Mapping(target = "contactInformationType", constant = "PHONE_NUMBER")
+    @Mapping(source = "baseAdditionalFields.createdDate", target = "baseAdditionalFieldsCreatedDate")
+    @Mapping(source = "baseAdditionalFields.updatedDate", target = "baseAdditionalFieldsUpdatedDate")
+    ContactInformationDto phoneNumberContactInformationToContactInformationDto(PhoneNumberContactInformation phoneNumberContactInformation);
 }
